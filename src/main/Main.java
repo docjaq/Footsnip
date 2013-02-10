f4090fcbf5cae5b5b7d2ba245da315a4dea326ae
David Payne <davecpayne@gmail.com>
2013-02-10 16:04:12 +0000

    Update 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,11 @@
-public class Main{
+public class Main {
 
-	public Main(){
+	public Main() {
 
 	}
 
-	public static void main(String[] args){
-		System.out.println("Hello Footsnip");
+	public static void main(String[] args) {
+		System.out.println("Hello Cuntsnip");
 		new Main();
 	}
 }
