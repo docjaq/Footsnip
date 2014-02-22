ddf9cc5e6d3fe37df0de610ec6ce1c498647deb0
docjaq <jonaquinn@gmail.com>
2014-02-22 12:00:32 +0000

    * Removed bug that stops collisions working when entities change tiles during a collision test

--- src/collision/CollisionThread.java
+++ src/collision/CollisionThread.java
@@ -7,7 +7,6 @@ import main.Main;
 import thread.GameThread;
 import assets.AssetContainer;
 import assets.entities.Entity;
-import assets.entities.Projectile;
 import assets.world.AbstractTile;
 import assets.world.datastructures.TileDataStructure2D;
 
@@ -17,40 +16,22 @@ public class CollisionThread extends GameThread {
 		super(assContainer, threadDelay, mainApplication);
 	}
 
-	// @Override
-	protected void gameLoopOld() {
-		// Put the player and monsters in a list
-		List<Entity> entities = new ArrayList<Entity>(assContainer.getMonsters().size() + assContainer.getProjectiles().size() + 1);
-		entities.add(assContainer.getPlayer());
-		entities.addAll(assContainer.getMonsters());
-
-		// If a projectile is ready, add it to the list
-		for (Projectile projectile : assContainer.getProjectiles()) {
-			if (projectile.readyForCollisionDetection()) {
-				entities.add(projectile);
-			}
-		}
-
-		// System.out.println("Computing " + entities.size() * entities.size() +
-		// " collisions");
-
-		// Pass list to pair-wise collision method
-		CollisionMethods.checkEntityCollisions(entities);
-	}
-
 	@Override
 	protected void gameLoop() {
 
 		TileDataStructure2D tiles = assContainer.getTileDataStructure();
-
-		int count = 0;
-
 		for (AbstractTile tile : tiles.getTilesAsList()) {
 
-			CollisionMethods.checkEntityCollisions(tile.getContainedEntities());
-			count += tile.getContainedEntities().size() * tile.getContainedEntities().size() - tile.getContainedEntities().size() / 2;
+			/*
+			 * Create a copy of this list before we send it to the collision
+			 * method. If we don't do this, if one of the entities in the list
+			 * changes tile, it will A) throw a null pointer exception in the
+			 * collision list, and B) not correctly test that entity for
+			 * collision!
+			 */
+			List<Entity> collisionCopyList = new ArrayList<Entity>(tile.getContainedEntities());
+			CollisionMethods.checkEntityCollisions(collisionCopyList);
+			collisionCopyList = null;
 		}
-
-		// System.out.println("Computing " + count + " collisions");
 	}
 }
