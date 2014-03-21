5bff9e51d79dcbf958e59f71d9933f6d5c516909
docjaq <jonaquinn@gmail.com>
2014-03-21 11:27:34 +0000

    * Cleaned up normal shader code

--- resources/shaders/tessellation/terrain_tessEval.glsl
+++ resources/shaders/tessellation/terrain_tessEval.glsl
@@ -18,9 +18,9 @@ uniform Projection
 	mat4 cameraToClipMatrix;
 };
 uniform sampler2D heightMap;
-//uniform sampler2D normalMap;
 
-//Using this to simulate the 8 possible offsets. Simply pull out values like .xx, .xz, etc. Saves a lot of local memory
+//Using this to simulate the 8 possible offsets. Simply pull out
+//values like .xx, .xz, etc. Saves a lot of local memory
 const ivec3 offset = ivec3(-1, 0, 1);
 
 vec3 computeNormal(vec2 co){
@@ -41,37 +41,30 @@ vec3 computeNormal(vec2 co){
     return normalize(normal);
 }
 
-void main()
-{
-    vec4 c0 = gl_TessCoord.x * tcDiffuseColor[0];
-    vec4 c1 = gl_TessCoord.y * tcDiffuseColor[1];
-    vec4 c2 = gl_TessCoord.z * tcDiffuseColor[2];
-    teDiffuseColor = (c0 + c1 + c2);
-    
+void main(){
+
+    //Compute position from tessellation
     vec3 p0 = gl_TessCoord.x * tcPosition[0];
     vec3 p1 = gl_TessCoord.y * tcPosition[1];
     vec3 p2 = gl_TessCoord.z * tcPosition[2];
     tePosition = vec3(p0 + p1 + p2);
-    //tePosition.z += (rand(vec2(tePosition))*0.04);
+
+    //Compute texture coords
     vec2 texCoordinates = vec2(tePosition);
     texCoordinates.x +=0.5;
     texCoordinates.y +=0.5;
+    
+    //Adjust position z according to heigtmap
     tePosition.z = (texture(heightMap, texCoordinates).r*2-1)*0.8-0.3;
-    
-    
-    //vec3 normal = (texture(normalMap, texCoordinates).rgb);
-    //This is strange that they need flipping
-    //normal.x= -(normal.x*2-1);
-    //normal.y= -(normal.y*2-1);
-    //normal.z= normal.z*2-1;
-    
 
+    //Compute normal and transform to camera coordinates
     teVertexNormal = normalModelToCameraMatrix * computeNormal(texCoordinates);
     
-    //Mess with vertex colors
+    //Compute vertex colours according to height
     teDiffuseColor.x=1+tePosition.z;
     teDiffuseColor.z-=tePosition.z;
     
+    //Transform position to camera coordinates
     vec4 tempCamPosition = modelToCameraMatrix * vec4(tePosition, 1.0);
     tePosition = vec3(tempCamPosition);
 	gl_Position = cameraToClipMatrix * tempCamPosition;
