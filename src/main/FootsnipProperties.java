1b388a1635a8d5e7bf4e36307f497388ee40527b
David Payne <david.payne@futurenet.com>
2014-04-12 19:16:29 +0100

    Adds default properties functionality.

--- src/main/FootsnipProperties.java
+++ src/main/FootsnipProperties.java
@@ -1,12 +1,15 @@
 package main;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class FootsnipProperties {
-	private static final String FILEPATH = "resources/conf/default.properties";
+	private static final String DEFAULT_FILEPATH = "resources/conf/default.properties";
+	private static final String LOCAL_FILEPATH = "resources/conf/local.properties";
+
 	private static Properties properties;
 
 	private synchronized static void init() {
@@ -18,20 +21,41 @@ public class FootsnipProperties {
 	private static void loadProperties() {
 		properties = new Properties();
 		try {
-			InputStream stream = new FileInputStream(FILEPATH);
-			properties.load(stream);
+			properties.load(getInputStream());
 		} catch (IOException e) {
-			throw new RuntimeException("Error loading properties file: " + FILEPATH, e);
+			throw new RuntimeException(e);
 		}
 	}
 
-	public static int getHeight() {
+	private static InputStream getInputStream() {
+		InputStream stream;
+		try {
+			stream = new FileInputStream(LOCAL_FILEPATH);
+		} catch (FileNotFoundException ex) {
+			try {
+				stream = new FileInputStream(DEFAULT_FILEPATH);
+			} catch (FileNotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		return stream;
+	}
+
+	private static int getIntProperty(String key) {
+		return Integer.parseInt(getProperty(key));
+	}
+
+	private static String getProperty(String key) {
 		init();
-		return Integer.parseInt(properties.getProperty("window.height"));
+		return properties.getProperty(key);
+	}
+
+	public static int getHeight() {
+		return getIntProperty("window.height");
 	}
 
 	public static int getWidth() {
-		init();
-		return Integer.parseInt(properties.getProperty("window.width"));
+		return getIntProperty("window.width");
 	}
 }
