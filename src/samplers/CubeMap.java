c8623ea1beb472bc5fc0c4c8e3a43b7814d9b54b
docjaq <jonaquinn@gmail.com>
2014-04-12 13:02:33 +0100

    * Texture fix because dave's machine is shit

--- src/samplers/CubeMap.java
+++ src/samplers/CubeMap.java
@@ -4,7 +4,6 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL30;
 import org.lwjgl.opengl.GL32;
-import org.lwjgl.opengl.GL42;
 
 import renderer.GLUtilityMethods;
 
@@ -22,10 +21,18 @@ public class CubeMap extends Texture {
 
 		GL11.glEnable(GL32.GL_TEXTURE_CUBE_MAP_SEAMLESS);
 
-		GL42.glTexStorage2D(GL13.GL_TEXTURE_CUBE_MAP, NUM_MIPMAPS, numColorChannels == 3 ? GL11.GL_RGB8 : GL11.GL_RGBA8, width, height);
+		// GL42.glTexStorage2D(GL13.GL_TEXTURE_CUBE_MAP, NUM_MIPMAPS,
+		// numColorChannels == 3 ? GL11.GL_RGB8 : GL11.GL_RGBA8, width, height);
+
 		for (int i = 0; i < 6; i++) {
-			GL11.glTexSubImage2D(GL13.GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, 0, 0, width, height, numColorChannels == 3 ? GL11.GL_RGB
-					: GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, GLUtilityMethods.loadPNG(textures[i], numColorChannels));
+			// GL11.glTexSubImage2D(GL13.GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0,
+			// 0, 0, width, height, numColorChannels == 3 ? GL11.GL_RGB
+			// : GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE,
+			// GLUtilityMethods.loadPNG(textures[i], numColorChannels));
+
+			GL11.glTexImage2D(GL13.GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, numColorChannels == 3 ? GL11.GL_RGB : GL11.GL_RGBA, width,
+					height, 0, numColorChannels == 3 ? GL11.GL_RGB : GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE,
+					GLUtilityMethods.loadPNG(textures[i], numColorChannels));
 		}
 		GL30.glGenerateMipmap(GL13.GL_TEXTURE_CUBE_MAP);
 
