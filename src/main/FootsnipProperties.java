e59156ca5c1e4e0a8ce895ff92d296658d8fad51
David Payne <david.payne@futurenet.com>
2014-04-12 18:51:16 +0100

    Extreme properties framework.

---
+++ src/main/FootsnipProperties.java
@@ -0,0 +1,37 @@
+package main;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class FootsnipProperties {
+	private static final String FILEPATH = "resources/conf/footsnip.properties";
+	private static Properties properties;
+
+	private synchronized static void init() {
+		if (properties == null) {
+			loadProperties();
+		}
+	}
+
+	private static void loadProperties() {
+		properties = new Properties();
+		try {
+			InputStream stream = new FileInputStream(FILEPATH);
+			properties.load(stream);
+		} catch (IOException e) {
+			throw new RuntimeException("Error loading properties file: " + FILEPATH, e);
+		}
+	}
+
+	public static int getHeight() {
+		init();
+		return Integer.parseInt(properties.getProperty("window.height"));
+	}
+
+	public static int getWidth() {
+		init();
+		return Integer.parseInt(properties.getProperty("window.width"));
+	}
+}
