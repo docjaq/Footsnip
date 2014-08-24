0d6ce564457120ce4f7882152be98e6598fcf590
David Payne <david.payne@propagatorgroup.com>
2014-08-24 13:19:15 +0100

    Pooling class.

---
+++ src/pooling/ObjectPool.java
@@ -0,0 +1,77 @@
+package pooling;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public abstract class ObjectPool<T>
+{
+    private ConcurrentLinkedQueue<T> pool;
+
+    private ScheduledExecutorService executorService;
+
+    public ObjectPool(final int minIdle) {
+        // initialize pool
+        initialize(minIdle);
+    }
+
+    public ObjectPool(final int minIdle, final int maxIdle, final long validationInterval) {
+        // initialize pool
+        initialize(minIdle);
+
+        // check pool conditions in a separate thread
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleWithFixedDelay(new Runnable()
+        {
+            @Override
+            public void run() {
+                int size = pool.size();
+                if (size < minIdle) {
+                    int sizeToBeAdded = minIdle - size;
+                    for (int i = 0; i < sizeToBeAdded; i++) {
+                        pool.add(createObject());
+                    }
+                } else if (size > maxIdle) {
+                    int sizeToBeRemoved = size - maxIdle;
+                    for (int i = 0; i < sizeToBeRemoved; i++) {
+                        pool.poll();
+                    }
+                }
+            }
+        }, validationInterval, validationInterval, TimeUnit.SECONDS);
+    }
+
+    public T borrowObject() {
+        T object;
+        if ((object = pool.poll()) == null) {
+            object = createObject();
+        }
+
+        return object;
+    }
+
+    public void returnObject(T object) {
+        if (object == null) {
+            return;
+        }
+
+        this.pool.offer(object);
+    }
+
+    public void shutdown() {
+        if (executorService != null) {
+            executorService.shutdown();
+        }
+    }
+
+    protected abstract T createObject();
+
+    private void initialize(final int minIdle) {
+        pool = new ConcurrentLinkedQueue<T>();
+
+        for (int i = 0; i < minIdle; i++) {
+            pool.add(createObject());
+        }
+    }
+}
