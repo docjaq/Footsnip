c3553b6f38eb4de2044877d02fb898532df05b01
David Payne <david.payne@propagatorgroup.com>
2014-12-02 00:04:38 +0000

    Properly exiting.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,35 +1,32 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-
+import assets.AssetContainer;
+import audio.AudioEngine;
+import control.ControlThread;
+import exception.RendererException;
 import location.LocationThread;
 import physics.PhysicsThread;
 import renderer.Renderer_4_0;
 import thread.GameThread;
 import thread.ObservableThread;
 import thread.ThreadObserver;
-import assets.AssetContainer;
-import audio.AudioEngine;
-import control.ControlThread;
-import exception.RendererException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Main implements GameListener {
 
-	private List<GameThread> childThreads = new ArrayList<GameThread>(5);
+	private List<GameThread> childThreads = new ArrayList<GameThread>(4);
 
 	private long startMillis = System.currentTimeMillis();
 
 	// TODO: define this threadpool size AFTER the threads have been defined so
 	// we don't have to hardcode a value
-	final ExecutorService executor = Executors.newFixedThreadPool(5);
+	ExecutorService executor = Executors.newFixedThreadPool(4);
 
-	/*********************************
+	/**
+	 * ******************************
 	 * JAQ Levels should maybe be entities, as it would seemingly make
 	 * intersections etc simpler to do (as you only ever have to intersect two
 	 * entities). Having said that, we might use special rules to intersect
@@ -41,6 +38,7 @@ public class Main implements GameListener {
 	public static void main(String[] args) {
 		try {
 			new Main();
+			System.exit(0);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			System.exit(-1);
@@ -72,11 +70,6 @@ public class Main implements GameListener {
 				GameThread controlThread = new ControlThread(assContainer, 10, Main.this);
 				childThreads.add(controlThread);
 				executor.execute(controlThread);
-
-				// GameThread collisionThread = new
-				// CollisionThread(assContainer, 10, Main.this);
-				// childThreads.add(collisionThread);
-				// executor.execute(collisionThread);
 
 				GameThread locationThread = new LocationThread(assContainer, 10, Main.this);
 				childThreads.add(locationThread);
@@ -139,52 +132,52 @@ public class Main implements GameListener {
 }
 
 /**
- * 
- * 
+ *
+ *
  * TODO: RENDERING:
- * 
+ *
  * - Seems sort of acceptable for now I think. Other more important things.
- * 
+ *
  * - Figure out how to do more passes on a single object. I.e. multiple lights,
  * procedural textures, etc. Think you can either call multiple shader programs,
  * or attach multiple shaders to a program. Going to need to read-up on this.
- * 
+ *
  * TODO: COLLISION:
- * 
+ *
  * - This is now a next step I guess. The one problem is it A) relies on a mesh
  * data-structure (see below), and B) on a scenegraph (see below)
- * 
+ *
  * TODO: SCENE-GRAPH:
- * 
+ *
  * - This is again a fair bit of work, but is pretty important for A)
  * hierarchies of stuff for lots of things (rendering, physics, collision, etc)
- * 
+ *
  * TODO: MESH STUFF:
- * 
+ *
  * - An easier way to define geometry. Soooo messy atm. Need to decide how we're
  * going to do this. I.e. just procedural algorithms, some loading/saving, etc.
- * 
+ *
  * - Some classes to create some basic Parametric (possibly?) mesh geometry
- * 
+ *
  * - A mesh data-structure. I'm for a half-edge one. It's overkill for gaming,
  * but will allow us to more easily do some cool stuff like: subdivision, CSG
  * (and other boolean-type operations). Judging by what I've found online, we
  * may need to write this ourselves...
- * 
+ *
  * - These are all really linked. 1) Define data-structure (DS) for meshes, 2)
  * Algorithms to create mesh. Put into DS, 3) Algorithms to put DS mesh into
  * opengl VBOs/VAOs, 4) Algorithms to correctly handle updating VBOs/VAOs when
  * mesh changes
- * 
+ *
  * TODO: PERSISTENCE:
- * 
+ *
  * - For defining a asteroid, game parameters. Perhaps JSON?
- * 
+ *
  * - Probably quite important so that we can get rid of some complexity in the
  * rendering/main methods?
- * 
+ *
  * TODO: PHYSICS:
- * 
+ *
  * - Our ship physics work, but are pretty basic. Not sure how to integrate more
  * into the rest of the game. More reading required.
  */
